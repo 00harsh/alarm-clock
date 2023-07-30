8ffa72e7c36c2901e60cdc4289212d71ef7b3070
00harsh <49751582+00harsh@users.noreply.github.com>
2023-07-30 13:18:05 +0530

    add script.js

---
+++ script.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const hourInput = document.getElementById('hourInput');
+    const minuteInput = document.getElementById('minuteInput');
+    const setAlarmBtn = document.getElementById('setAlarmBtn');
+    let alarmInterval;
+  
+    setAlarmBtn.addEventListener('click', function () {
+      const hours = parseInt(hourInput.value);
+      const minutes = parseInt(minuteInput.value);
+  
+      if (isNaN(hours) || isNaN(minutes)) {
+        alert('Please enter valid hour and minute values.');
+        return;
+      }
+  
+      const now = new Date();
+      const alarmTime = new Date(now);
+      alarmTime.setHours(hours, minutes, 0, 0);
+  
+      const timeDifference = alarmTime - now;
+  
+      if (timeDifference <= 0) {
+        alert('Please choose a time in the future.');
+        return;
+      }
+  
+      clearInterval(alarmInterval);
+  
+      alarmInterval = setInterval(function () {
+        const currentTime = new Date();
+        const remainingTime = alarmTime - currentTime;
+  
+        if (remainingTime <= 0) {
+          clearInterval(alarmInterval);
+          triggerAlarm();
+        }
+      }, 1000);
+  
+      alert('Alarm set successfully!');
+    });
+  
+    function triggerAlarm() {
+      // You can customize the action when the alarm is triggered (e.g., play a sound, show a notification)
+      alert('ALARM! Wake up!');
+    }
+  });
+  
